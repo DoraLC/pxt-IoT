ab301ffb99c11f72a5b8a2a46d349289cae1ca96
Wong Wai Chung <simon19950213@gmail.com>
2018-05-14 15:01:59 +0800

    init

--- main.ts
+++ main.ts
@@ -26,30 +26,34 @@ namespace ESP8266 {
 	//% weight=70	
 	//% blockGap=7	
     export function sendThingspeak(key: string, field1: number): void {
-    	let message = "/update?api_key=" + key + "&field1=" + field1 + "\r\n\r\n";
+    	let message = "GET /update?api_key=" + key + "&field1=" + field1 + "\r\n\r\n";
+    	serial.writeString("AT+CIPMUX=0\r\n");
+    	basic.pause(100);
     	serial.writeString("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n");
-    	basic.pause(100)
+    	basic.pause(100);
     	serial.writeString("AT+CIPSEND=" + message.length + "\r\n");
-    	basic.pause(100)
-    	serial.writeString("GET " + message);
-    	basic.pause(100)
+    	basic.pause(100);
+    	serial.writeString(message);
+    	basic.pause(100);
     	serial.writeString("AT+CIPCLOSE\r\n");
-    	basic.pause(100)
+    	basic.pause(100);
     }
 	
     //% blockId=esp8266_set_ifttt
 	//% block="Send IFTTT key %key|event_name %eventname|value1 %value1|value2 %value2"
 	//% weight=60	
     export function sendIFTTT(key: string, eventname: string, value1: number, value2: number): void {
-    	let message = "/trigger/" + eventname + "/with/key/" + key + "?value1=" + value1 + "&value2=" + value2 + "\r\n\r\n";
+    	let message = "GET /trigger/" + eventname + "/with/key/" + key + "?value1=" + value1 + "&value2=" + value2 + "\r\n\r\n";
+       	serial.writeString("AT+CIPMUX=0\r\n");
+       	basic.pause(100);
     	serial.writeString("AT+CIPSTART=\"TCP\",\"maker.ifttt.com\",80\r\n");
-    	basic.pause(100)
+    	basic.pause(100);
     	serial.writeString("AT+CIPSEND=" + message.length + "\r\n");
-    	basic.pause(100)
-    	serial.writeString("GET " + message);
-    	basic.pause(100)
+    	basic.pause(100);
+    	serial.writeString(message);
+    	basic.pause(100);
     	serial.writeString("AT+CIPCLOSE\r\n");
-    	basic.pause(100)
+    	basic.pause(100);
     }
 
 }
