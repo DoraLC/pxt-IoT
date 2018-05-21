d9fe83419303cb363b3cae23ec20298a1ec69440
Wong Wai Chung <simon19950213@gmail.com>
2018-05-21 19:54:13 +0800

    update

--- main.ts
+++ main.ts
@@ -1,59 +1,50 @@
 //% weight=100 color=#F59E20 icon="\uf1eb" block="esp8266"
 namespace ESP8266 {
-	// -------------- 1. Initialization ----------------
+    // -------------- 1. Initialization ----------------
     //%blockId=esp8266_initialize_wifi
     //%block="Initialize WiFi"
-	//% weight=90	
-	//% blockGap=7	
+    //% weight=90   
+    //% blockGap=7  
     export function initializeWifi(): void {
         serial.redirect(SerialPin.P16,SerialPin.P8,BaudRate.BaudRate115200);
         serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {});
     }
-	
-	// -------------- 2. WiFi ----------------
+    
+    // -------------- 2. WiFi ----------------
     //% blockId=esp8266_set_wifi
-	//% block="Set wifi to ssid %ssid| pwd %pwd"   
-	//% weight=80		
-	//% blockGap=7	
+    //% block="Set wifi to ssid %ssid| pwd %pwd"   
+    //% weight=80       
+    //% blockGap=7  
     export function setWifi(ssid: string, pwd: string): void {
-    	serial.writeString("AT+RST\r\n");
-    	serial.writeString("AT+CWMODE=1\r\n");
+        serial.writeString("AT+RST\r\n");
+        serial.writeString("AT+CWMODE=1\r\n");
         serial.writeString("AT+CWJAP=\"" + ssid + "\",\"" + pwd + "\"\r\n");
     }
 
-	// -------------- 3. Cloud ----------------
+    // -------------- 3. Cloud ----------------
     //% blockId=esp8266_set_thingspeak
-	//% block="Send ThingSpeak key %key| field1 %field1"
-	//% weight=70	
-	//% blockGap=7	
+    //% block="Send ThingSpeak key %key| field1 %field1"
+    //% weight=70   
+    //% blockGap=7  
     export function sendThingspeak(key: string, field1: number): void {
-    	let message = "GET /update?api_key=" + key + "&field1=" + field1 + "\r\n\r\n";
-    	serial.writeString("AT+CIPMUX=0\r\n");
-    	serial.writeString("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n");
-    	serial.writeString("AT+CIPSEND=" + message.length + "\r\n");
-    	serial.writeString(message);
-    	serial.writeString("AT+CIPCLOSE\r\n");
+        let message = "GET /update?api_key=" + key + "&field1=" + field1 + "\r\n\r\n";
+        serial.writeString("AT+CIPMUX=0\r\n");
+        serial.writeString("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n");
+        serial.writeString("AT+CIPSEND=" + message.length + "\r\n");
+        serial.writeString(message);
+        serial.writeString("AT+CIPCLOSE\r\n");
     }
-	
-    //% blockId=esp8266_init_ifttt
-    //% block="Initialize IFTTT"
+    
+    //% blockId=esp8266_set_ifttt
+    //% block="Send IFTTT key %key|event_name %eventname|value1 %value1|value2 %value2"
     //% weight=60   
-    export function initializeIFTTT(): void {
-        basic.pause(1000);
+    export function sendIFTTT(key: string, eventname: string, value1: number, value2: number): void {
+        let message = "GET /trigger/" + eventname + "/with/key/" + key + "?value1=" + value1 + "&value2=" + value2 + " HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n";
         serial.writeString("AT+CIPMUX=0\r\n");
         serial.writeString("AT+CIPSTART=\"TCP\",\"maker.ifttt.com\",80\r\n");
-    }
-
-    //% blockId=esp8266_set_ifttt
-	//% block="Send IFTTT key %key|event_name %eventname|value1 %value1|value2 %value2"
-	//% weight=60	
-    export function sendIFTTT(key: string, eventname: string, value1: number, value2: number): void {
-    	let message = "GET /trigger/" + eventname + "/with/key/" + key + "?value1=" + value1 + "&value2=" + value2 + " HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n";
-       	// serial.writeString("AT+CIPMUX=0\r\n");
-    	// serial.writeString("AT+CIPSTART=\"TCP\",\"maker.ifttt.com\",80\r\n");
-    	serial.writeString("AT+CIPSEND=" + message.length + "\r\n");
-    	serial.writeString(message);
-    	// serial.writeString("AT+CIPCLOSE\r\n");
+        serial.writeString("AT+CIPSEND=" + message.length + "\r\n");
+        serial.writeString(message);
+        serial.writeString("AT+CIPCLOSE\r\n");
     }
 
 }
