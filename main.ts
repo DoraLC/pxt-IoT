08963aadf64ee93f169287db92f56bc9e65edcc1
Wong Wai Chung <simon19950213@gmail.com>
2018-05-24 11:33:26 +0800

    update

--- main.ts
+++ main.ts
@@ -7,7 +7,8 @@ namespace ESP8266 {
     //% tx.fieldOptions.tooltips="false"
     //% rx.fieldEditor="gridpicker" rx.fieldOptions.columns=3
     //% rx.fieldOptions.tooltips="false"
-    //% weight=60   
+    //% weight=80   
+    //% blockGap=7  
     export function initializeWifi(tx: SerialPin, rx:SerialPin): void {
         serial.redirect(tx,rx,BaudRate.BaudRate115200);
         serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {});
@@ -27,7 +28,8 @@ namespace ESP8266 {
     // -------------- 3. Cloud ----------------
     //% blockId=esp8266_set_thingspeak
     //% block="Send ThingSpeak key %key| field1 %field1"
-    //% weight=70
+    //% weight=80
+    //% blockGap=7  
     export function sendThingspeak(key: string, field1: number): void {
         let message = "GET /update?api_key=" + key + "&field1=" + field1 + "\r\n\r\n";
         serial.writeString("AT+CIPMUX=0\r\n");
@@ -39,7 +41,8 @@ namespace ESP8266 {
     
     //% blockId=esp8266_set_ifttt
     //% block="Send IFTTT key %key|event_name %eventname|value1 %value1|value2 %value2"
-    //% weight=60   
+    //% weight=80   
+    //% blockGap=7  
     export function sendIFTTT(key: string, eventname: string, value1: number, value2: number): void {
         let message = "GET /trigger/" + eventname + "/with/key/" + key + "?value1=" + value1 + "&value2=" + value2 + " HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n";
         serial.writeString("AT+CIPMUX=0\r\n");
