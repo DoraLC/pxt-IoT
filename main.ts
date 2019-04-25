b02d60eb9bd2c3a8ac5e55652519e416273d52aa
DoraLC <34760377+DoraLC@users.noreply.github.com>
2019-04-25 15:36:16 +0800

    update main.ts

--- main.ts
+++ main.ts
@@ -57,7 +57,7 @@ namespace ESP8266 {
     export function setWifi(ssid: string, pwd: string, mqtton: boolean): void {
         serial.writeString("AT+RST\r\n");
         if (mqtton){
-            basic.pause(3000);
+            basic.pause(1000);
             serial.writeString("AT+MQRES\r\n");
         }
         basic.pause(1500);
