63962595955a24a2b4816264d16215f106fb36e5
DoraLC <34760377+DoraLC@users.noreply.github.com>
2019-05-24 16:56:29 +0800

    fix bugs

--- main.ts
+++ main.ts
@@ -54,10 +54,10 @@ namespace ESP8266 {
                 tmpmessage(serial_str);
             }
             if (mqttflag) {
-                serial_str = serial.readLine();
                 let mqttmeg_pos: number = serial_str.indexOf(mqtt_topic) + mqtt_topic.length
                 let mqttmeg: string = serial_str.substr(mqttmeg_pos + 3, 100)
                 //mqttmessage(mqttmeg);
+                serial_str = serial.readLine();
                 mqttmeg += serial_str;
                 mqttCallback(mqtt_topic, mqttmeg);
                 mqttflag = false;
